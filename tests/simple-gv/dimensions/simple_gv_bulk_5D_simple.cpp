357bd3714e1b742c08725d542fe872fafcec49a3
Derek Guscott <derek.guscott@wsu.edu>
2022-05-20 13:27:20 -0700

    4-9D preliminary tests
    Implemented basic tests for 4-9Ds based off of the existing simple_gv_bulk_nD_simple.cpp file. Theses tests still need to be fleshed out.
    
    Co-Authored-By: Nathan Balcarcel <98621289+nbalcarc@users.noreply.github.com>

---
+++ tests/simple-gv/dimensions/simple_gv_bulk_5D_simple.cpp
@@ -0,0 +1,59 @@
+#include "rmacxx.hpp"
+
+int main(int argc, char *argv[])
+{
+    int rank;
+
+    MPI_Init( &argc, &argv );
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+    
+    std::vector<int> dims(5), pgrid(5);
+
+    // create window
+    if (rank == 0) // process #0
+    { 
+        dims[0] = 2; dims[1] = 2; dims[2] = 2; dims[3] = 2; dims[4] = 2;
+        pgrid[0] = 0; pgrid[1] = 0; pgrid[2] = 0; pgrid[3] = 0; pgrid[4] = 0;
+    }
+    else // process #1
+    {
+        dims[0] = 2; dims[1] = 2; dims[2] = 2; dims[3] = 2; dims[4] = 2;
+        pgrid[0] = 0; pgrid[1] = 1; pgrid[2] = 0; pgrid[3] = 0; pgrid[4] = 0;
+    }
+           
+    rmacxx::Window<int,GLOBAL_VIEW> win(dims, pgrid);
+
+    if (win.size() != 2)
+    {
+        std::cout << "Number of processes should be exactly 2. Aborting..." << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, -99);
+    }
+
+    // print data ranges per process
+    win.print_ranges();
+
+    // fill window buffer
+    win.fill(1);
+
+    // print shape
+    win.print("Current...");
+    
+    // put
+    std::vector<int> data(32);
+    for(int i = 0; i < 32; i++)
+        data[i] = 3;
+
+    win({0,1},{1,2}) << data.data();
+    
+    win.flush();
+    
+    win.print("After put...");
+
+    win.barrier();
+    
+    win.wfree();
+
+    MPI_Finalize();
+
+    return 0;
+}
