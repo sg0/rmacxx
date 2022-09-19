db47f009bacfc5433ed7780e23190a8e28e7a0ac
Marcel Mukundi <marcel.mukundi@wsu.edu>
2022-09-18 19:16:17 -0700

    Update build styem
    Made CI/CD system run on all branches
    Updated makefile to run multiple processes
    Added test badges

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,3 @@
-mpic++ $1.cpp -O3 -Wall -std=c++17 -pthread -I./include -D_GNU_SOURCE -DTEST_OVERHEAD -o $1;
-mpiexec -np $2 $1;
+mpic++ $1.cpp -O3 -Wall -std=c++17 -pthread -I./include -D_GNU_SOURCE -DTEST_OVERHEAD -o $1 &&
+mpiexec -np $2 $1 &&
 rm $1;
