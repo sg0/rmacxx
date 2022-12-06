f12edfd7d4d2612862e98380387468c8c9553bf7
Mukundi_ Marcel <marcel.mukundi@login-p2n04.kamiak.wsu.edu>
2022-12-06 15:09:54 -0800

    Setup for demo

---
+++ CTestTestfile.cmake
@@ -0,0 +1,38 @@
+# CMake generated Testfile for 
+# Source directory: /home/marcel.mukundi/rmacxx
+# Build directory: /home/marcel.mukundi/rmacxx
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(bench_ncontig_data.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_ncontig_data.cpp")
+set_tests_properties(bench_ncontig_data.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_prmabwtest.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_prmabwtest.cpp")
+set_tests_properties(bench_prmabwtest.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_acc.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_acc.cpp")
+set_tests_properties(bench_simple_acc.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_cas.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_cas.cpp")
+set_tests_properties(bench_simple_cas.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_expr.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_expr.cpp")
+set_tests_properties(bench_simple_expr.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_expr2.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_expr2.cpp")
+set_tests_properties(bench_simple_expr2.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_fop.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_fop.cpp")
+set_tests_properties(bench_simple_fop.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_gacc.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_gacc.cpp")
+set_tests_properties(bench_simple_gacc.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_get.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_get.cpp")
+set_tests_properties(bench_simple_get.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_put.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_put.cpp")
+set_tests_properties(bench_simple_put.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_put_bulk.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_put_bulk.cpp")
+set_tests_properties(bench_simple_put_bulk.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_put_concurrent.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_put_concurrent.cpp")
+set_tests_properties(bench_simple_put_concurrent.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_put_gv.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_put_gv.cpp")
+set_tests_properties(bench_simple_put_gv.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_put_gv_strided.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_put_gv_strided.cpp")
+set_tests_properties(bench_simple_put_gv_strided.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_put_strided.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_put_strided.cpp")
+set_tests_properties(bench_simple_put_strided.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
+add_test(bench_simple_put_strided_2D.cpp "mpiexec" "--np" "20" "/home/marcel.mukundi/rmacxx/bench_simple_put_strided_2D.cpp")
+set_tests_properties(bench_simple_put_strided_2D.cpp PROPERTIES  _BACKTRACE_TRIPLES "/home/marcel.mukundi/rmacxx/CMakeLists.txt;36;add_test;/home/marcel.mukundi/rmacxx/CMakeLists.txt;0;")
---
+++ DartConfiguration.tcl
@@ -0,0 +1,107 @@
+# This file is configured by CMake automatically as DartConfiguration.tcl
+# If you choose not to use CMake, this file may be hand configured, by
+# filling in the required variables.
+
+
+# Configuration directories and files
+SourceDirectory: /home/marcel.mukundi/rmacxx
+BuildDirectory: /home/marcel.mukundi/rmacxx
+
+# Where to place the cost data store
+CostDataFile: 
+
+# Site is something like machine.domain, i.e. pragmatic.crd
+Site: login-p2n04.kamiak.wsu.edu
+
+# Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
+BuildName: Linux-mpicxx
+
+# Subprojects
+LabelsForSubprojects: 
+
+# Submission information
+SubmitURL: http://
+
+# Dashboard start time
+NightlyStartTime: 00:00:00 EDT
+
+# Commands for the build/test/submit cycle
+ConfigureCommand: "/weka/apps/cmake/3.17.0/intel/20.0/bin/cmake" "/home/marcel.mukundi/rmacxx"
+MakeCommand: /usr/bin/gmake -i
+DefaultCTestConfigurationType: Release
+
+# version control
+UpdateVersionOnly: 
+
+# CVS options
+# Default is "-d -P -A"
+CVSCommand: CVSCOMMAND-NOTFOUND
+CVSUpdateOptions: -d -A -P
+
+# Subversion options
+SVNCommand: SVNCOMMAND-NOTFOUND
+SVNOptions: 
+SVNUpdateOptions: 
+
+# Git options
+GITCommand: /usr/bin/git
+GITInitSubmodules: 
+GITUpdateOptions: 
+GITUpdateCustom: 
+
+# Perforce options
+P4Command: P4COMMAND-NOTFOUND
+P4Client: 
+P4Options: 
+P4UpdateOptions: 
+P4UpdateCustom: 
+
+# Generic update command
+UpdateCommand: /usr/bin/git
+UpdateOptions: 
+UpdateType: git
+
+# Compiler info
+Compiler: mpicxx
+CompilerVersion: 7.3.0
+
+# Dynamic analysis (MemCheck)
+PurifyCommand: 
+ValgrindCommand: 
+ValgrindCommandOptions: 
+DrMemoryCommand: 
+DrMemoryCommandOptions: 
+MemoryCheckType: 
+MemoryCheckSanitizerOptions: 
+MemoryCheckCommand: MEMORYCHECK_COMMAND-NOTFOUND
+MemoryCheckCommandOptions: 
+MemoryCheckSuppressionFile: 
+
+# Coverage
+CoverageCommand: /opt/apps/gcc/7.3.0/bin/gcov
+CoverageExtraFlags: -l
+
+# Cluster commands
+SlurmBatchCommand: /opt/apps/system/scripts/sbatch
+SlurmRunCommand: /usr/bin/srun
+
+# Testing options
+# TimeOut is the amount of time in seconds to wait for processes
+# to complete during testing.  After TimeOut seconds, the
+# process will be summarily terminated.
+# Currently set to 25 minutes
+TimeOut: 1500
+
+# During parallel testing CTest will not start a new test if doing
+# so would cause the system load to exceed this value.
+TestLoad: 
+
+UseLaunchers: 
+CurlOptions: 
+# warning, if you add new options here that have to do with submit,
+# you have to update cmCTestSubmitCommand.cxx
+
+# For CTest submissions that timeout, these options
+# specify behavior for retrying the submission
+CTestSubmitRetryDelay: 5
+CTestSubmitRetryCount: 3
